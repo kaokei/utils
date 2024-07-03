5a2d5b96f62692ddcba3c02d5e5ace620507c332
kaokei <kaokei.rst@gmail.com>
2024-07-03 10:32:01 +0800

    实现基本逻辑

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,141 @@
-export const one = 1
-export const two = 2
+export interface Option {
+  key: PropertyKey
+  value: string | number
+  label: string
+  [propName: string]: any // 允许任意自定义属性
+}
+
+type ToKeys<T> = {
+  [P in keyof T]: T[P] extends { key: infer K } ? K : never
+}
+
+type ToValues<T> = {
+  [P in keyof T]: T[P] extends { value: infer K } ? K : never
+}
+
+type KeyValueMap<T extends readonly Option[]> = {
+  [P in T[number]['key']]: Extract<T[number], { key: P }>['value']
+}
+
+type CustomKeyMap<T extends readonly Option[], K extends keyof Option> = {
+  [P in T[number]['key']]: Extract<T[number], { key: P }>[K]
+}
+
+type KeyLabelMap<T extends readonly Option[]> = {
+  [P in T[number]['key']]: Extract<T[number], { key: P }>['label']
+}
+
+type ValueKeyMap<T extends readonly Option[]> = {
+  [P in T[number]['value']]: Extract<T[number], { key: P }>['key']
+}
+
+type CustomValueMap<T extends readonly Option[], K extends keyof Option> = {
+  [P in T[number]['value']]: Extract<T[number], { value: P }>[K]
+}
+
+type ValueLabelMap<T extends readonly Option[]> = {
+  [P in T[number]['value']]: Extract<T[number], { key: P }>['label']
+}
+
+type KeyMap<T extends readonly Option[]> = {
+  [P in T[number]['key']]: Extract<T[number], { key: P }>
+}
+
+type ValueMap<T extends readonly Option[]> = {
+  [P in T[number]['value']]: Extract<T[number], { key: P }>
+}
+
+/**
+ * 获取所有 key
+ */
+export function getKeys<T extends readonly Option[]>(options: T) {
+  return options.map(option => option.key) as ToKeys<T>
+}
+
+/**
+ * 获取所有 value
+ */
+export function getValues<T extends readonly Option[]>(options: T) {
+  return options.map(option => option.value) as ToValues<T>
+}
+
+/**
+ * 获取key-value映射
+ */
+export function getKeyValueMap<T extends readonly Option[]>(options: T) {
+  return options.reduce((map, option) => ({
+    ...map,
+    [option.key]: option.value,
+  }), {} as KeyValueMap<T>)
+}
+
+/**
+ * 获取key-label映射
+ */
+export function getKeyLabelMap<T extends readonly Option[]>(options: T) {
+  return options.reduce((map, option) => ({
+    ...map,
+    [option.key]: option.label,
+  }), {} as KeyLabelMap<T>)
+}
+
+/**
+ * 获取value-key映射
+ */
+export function getValueKeyMap<T extends readonly Option[]>(options: T) {
+  return options.reduce((map, option) => ({
+    ...map,
+    [option.value]: option.key,
+  }), {} as ValueKeyMap<T>)
+}
+
+/**
+ * 获取value-label映射
+ */
+export function getValueLabelMap<T extends readonly Option[]>(options: T) {
+  return options.reduce((map, option) => ({
+    ...map,
+    [option.value]: option.label,
+  }), {} as ValueLabelMap<T>)
+}
+
+/**
+ * 获取key-option映射
+ */
+export function getKeyMap<T extends readonly Option[]>(options: T) {
+  return options.reduce((map, option) => ({
+    ...map,
+    [option.key]: option,
+  }), {} as KeyMap<T>)
+}
+
+/**
+ * 获取value-option映射
+ */
+export function getValueMap<T extends readonly Option[]>(options: T) {
+  return options.reduce((map, option) => ({
+    ...map,
+    [option.value]: option,
+  }), {} as ValueMap<T>)
+}
+
+/**
+ * 自定义 key-指定属性映射
+ */
+
+export function getMapByKey<T extends readonly Option[], K extends keyof Option>(options: T, valueKey: K) {
+  return options.reduce((map, option) => ({
+    ...map,
+    [option.key]: option[valueKey],
+  }), {} as CustomKeyMap<T, K>)
+}
+
+/**
+ * 自定义 value-指定属性映射
+ */
+export function getMapByValue<T extends readonly Option[], K extends keyof Option>(options: T, valueKey: K) {
+  return options.reduce((map, option) => ({
+    ...map,
+    [option.value]: option[valueKey],
+  }), {} as CustomValueMap<T, K>)
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,109 @@
 import { describe, expect, it } from 'vitest'
 
-describe('should', () => {
-  it('exported', () => {
-    expect(1).toEqual(1)
+import {
+  getKeyLabelMap,
+  getKeyMap,
+  getKeyValueMap,
+  getKeys,
+  getMapByKey,
+  getMapByValue,
+  getValueKeyMap,
+  getValueLabelMap,
+  getValueMap,
+  getValues,
+} from '../src/index'
+
+export const EXAMPLE_OPTIONS = [
+  {
+    key: 'BOY', // 唯一性
+    value: 1, // 唯一性
+    label: '男生',
+    tag: '高达',
+    color: 'red',
+  },
+  {
+    key: 'GIRL', // 唯一性
+    value: 2, // 唯一性
+    label: '女生',
+    tag: '小仙女',
+    color: 'green',
+  },
+] as const
+
+describe('typeScript type tests', () => {
+  it('getKeys', () => {
+    const keys = getKeys(EXAMPLE_OPTIONS)
+
+    expect(keys).toEqual(['BOY', 'GIRL'])
+  })
+
+  it('getValues', () => {
+    const values = getValues(EXAMPLE_OPTIONS)
+
+    expect(values).toEqual([1, 2])
+  })
+
+  it('getKeyValueMap', () => {
+    const keyValueMap = getKeyValueMap(EXAMPLE_OPTIONS)
+
+    expect(keyValueMap.BOY).toBe(1)
+    expect(keyValueMap.GIRL).toBe(2)
+    expect(keyValueMap).toEqual({ BOY: 1, GIRL: 2 })
+  })
+
+  it('getKeyLabelMap', () => {
+    const keyLabelMap = getKeyLabelMap(EXAMPLE_OPTIONS)
+
+    expect(keyLabelMap.BOY).toBe('男生')
+    expect(keyLabelMap.GIRL).toBe('女生')
+    expect(keyLabelMap).toEqual({ BOY: '男生', GIRL: '女生' })
+  })
+
+  it('getValueKeyMap', () => {
+    const valueKeyMap = getValueKeyMap(EXAMPLE_OPTIONS)
+
+    expect(valueKeyMap[1]).toBe('BOY')
+    expect(valueKeyMap[2]).toBe('GIRL')
+    expect(valueKeyMap).toEqual({ 1: 'BOY', 2: 'GIRL' })
+  })
+
+  it('getValueLabelMap', () => {
+    const valueLabelMap = getValueLabelMap(EXAMPLE_OPTIONS)
+
+    expect(valueLabelMap[1]).toBe('男生')
+    expect(valueLabelMap[2]).toBe('女生')
+    expect(valueLabelMap).toEqual({ 1: '男生', 2: '女生' })
+  })
+
+  it('getKeyMap', () => {
+    const keyMap = getKeyMap(EXAMPLE_OPTIONS)
+
+    expect(keyMap.BOY).toBe(EXAMPLE_OPTIONS[0])
+    expect(keyMap.GIRL).toBe(EXAMPLE_OPTIONS[1])
+    expect(keyMap).toEqual({ BOY: EXAMPLE_OPTIONS[0], GIRL: EXAMPLE_OPTIONS[1] })
+  })
+
+  it('getValueMap', () => {
+    const valueMap = getValueMap(EXAMPLE_OPTIONS)
+
+    expect(valueMap[1]).toBe(EXAMPLE_OPTIONS[0])
+    expect(valueMap[2]).toBe(EXAMPLE_OPTIONS[1])
+    expect(valueMap).toEqual({ 1: EXAMPLE_OPTIONS[0], 2: EXAMPLE_OPTIONS[1] })
+  })
+
+  it('getMapByKey', () => {
+    const keyMap = getMapByKey(EXAMPLE_OPTIONS, 'tag')
+
+    expect(keyMap.BOY).toBe('高达')
+    expect(keyMap.GIRL).toBe('小仙女')
+    expect(keyMap).toEqual({ BOY: '高达', GIRL: '小仙女' })
+  })
+
+  it('getMapByValue', () => {
+    const valueMap = getMapByValue(EXAMPLE_OPTIONS, 'color')
+
+    expect(valueMap[1]).toBe('red')
+    expect(valueMap[2]).toBe('green')
+    expect(valueMap).toEqual({ 1: 'red', 2: 'green' })
   })
 })
